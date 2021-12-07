c1a2c7d5d9a291e46aca3ebf8afb38d29a3caf37
Rakesh <iamrakeshkumar@pm.me>
2021-12-07 23:27:07 +0530

    style:product bar responsive

--- src/view/Products/ProductBar/index.jsx
+++ src/view/Products/ProductBar/index.jsx
@@ -2,75 +2,75 @@ import { useControl } from "../../../hooks";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import CloseIcon from "@material-ui/icons/Close";
 import style from "./ProductBar.module.css";
-// import SearchIcon from "@mui/icons-material/Search";
+import MediaQuery from "react-responsive";
 
 export const ProductBar = ({
   showFilterModal,
   setShowFilterModal,
   filterByCategory,
 }) => {
-  // const [active, setActive] = useState('All')
-
   const { filterGloves, filterHelmets, filterJackets, filterShoes, showAll } =
     useControl();
 
   return (
     <div className="container">
       <div className={style.product_bar}>
-        <div className="flex-col-xl-10 flex-col-lg-10 flex-col-md-8 flex-col-sm-8 text-left">
-          <span
-            className={
-              filterByCategory === "ALL"
-                ? style.product_bar_item_active
-                : style.product_bar_item
-            }
-            onClick={showAll}
-          >
-            All
-          </span>
-          <span
-            className={
-              filterByCategory === "JACKETS"
-                ? style.product_bar_item_active
-                : style.product_bar_item
-            }
-            onClick={filterJackets}
-          >
-            Jackets
-          </span>
-          <span
-            className={
-              filterByCategory === "HELMETS"
-                ? style.product_bar_item_active
-                : style.product_bar_item
-            }
-            onClick={filterHelmets}
-          >
-            Helmets
-          </span>
-          <span
-            className={
-              filterByCategory === "GLOVES"
-                ? style.product_bar_item_active
-                : style.product_bar_item
-            }
-            onClick={filterGloves}
-          >
-            Gloves
-          </span>
-          <span
-            className={
-              filterByCategory === "SHOES"
-                ? style.product_bar_item_active
-                : style.product_bar_item
-            }
-            onClick={filterShoes}
-          >
-            Shoes
-          </span>
+        <div className="flex-col-xl-10 flex-col-lg-10 flex-col-md-11 flex-col-sm-11 text-left">
+          <div className={style.product_bar_item_container}>
+            <span
+              className={
+                filterByCategory === "ALL"
+                  ? style.product_bar_item_active
+                  : style.product_bar_item
+              }
+              onClick={showAll}
+            >
+              All
+            </span>
+            <span
+              className={
+                filterByCategory === "JACKETS"
+                  ? style.product_bar_item_active
+                  : style.product_bar_item
+              }
+              onClick={filterJackets}
+            >
+              Jackets
+            </span>
+            <span
+              className={
+                filterByCategory === "HELMETS"
+                  ? style.product_bar_item_active
+                  : style.product_bar_item
+              }
+              onClick={filterHelmets}
+            >
+              Helmets
+            </span>
+            <span
+              className={
+                filterByCategory === "GLOVES"
+                  ? style.product_bar_item_active
+                  : style.product_bar_item
+              }
+              onClick={filterGloves}
+            >
+              Gloves
+            </span>
+            <span
+              className={
+                filterByCategory === "SHOES"
+                  ? style.product_bar_item_active
+                  : style.product_bar_item
+              }
+              onClick={filterShoes}
+            >
+              Shoes
+            </span>
+          </div>
         </div>
 
-        <div className="flex-col-xl-2 flex-col-lg-2 flex-col-md-4 flex-col-sm-4">
+        <div className="flex-col-xl-2 flex-col-lg-2 flex-col-md-1 flex-col-sm-1 flex-col-1">
           <div
             onClick={() =>
               setShowFilterModal((showFilterModal) => !showFilterModal)
@@ -80,31 +80,15 @@ export const ProductBar = ({
             }
           >
             {showFilterModal ? (
-              <CloseIcon fontSize="small" style={{ marginRight: ".5rem" }} />
+              <CloseIcon fontSize="small" />
             ) : (
-              <FilterListIcon
-                fontSize="small"
-                style={{ marginRight: ".5rem" }}
-              />
+              <FilterListIcon fontSize="small" />
             )}
-            <span>Sort & Filter </span>
+            <MediaQuery minWidth={1224}>
+              <span style={{ marginLeft: ".5rem" }}>Sort & Filter </span>
+            </MediaQuery>
           </div>
         </div>
-        {/* <div className="flex-col-xl-2 flex-col-lg-2 flex-col-md-4 flex-col-sm-4">
-          <div
-            onClick={() =>
-              setShowFilterModal((showFilterModal) => !showFilterModal)
-            }
-            className={showFilterModal ? "filter-btn-active" : "filter-btn"}
-          >
-            {showFilterModal ? (
-              <CloseIcon fontSize="small" style={{ marginRight: ".5rem" }} />
-            ) : (
-              <SearchIcon fontSize="small" style={{ marginRight: ".5rem" }} />
-            )}
-            <span>Search </span>
-          </div>
-        </div> */}
       </div>
     </div>
   );
