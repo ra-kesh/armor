90979bff3dc1e22de612e58ecd0e2d969bfb77f1
Rakesh <iamrakeshkumar@pm.me>
2021-12-07 21:20:40 +0530

    style:desktopbar

--- src/component/Nav/DesktopBar/index.jsx
+++ src/component/Nav/DesktopBar/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../../hooks";
+import { useAuth } from "../../../hooks";
+import style from "./DesktopBar.module.css";
 
 const DesktopBar = () => {
   const { userInfo } = useAuth();
@@ -8,13 +9,13 @@ const DesktopBar = () => {
   return (
     <ul className="block">
       <Link to="/">
-        <li>Home</li>
+        <li className={style.desktop_bar_item}>Home</li>
       </Link>
       <Link to="/products">
-        <li>Products</li>
+        <li className={style.desktop_bar_item}>Products</li>
       </Link>
       <Link to={userInfo ? "/user" : "/login"}>
-        <li>Account</li>
+        <li className={style.desktop_bar_item}>Account</li>
       </Link>
     </ul>
   );
