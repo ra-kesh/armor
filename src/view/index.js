cf7265f3da5f02a81dee306053b892cb3206155f
Rakesh <iamrakeshkumar@pm.me>
2022-03-08 15:21:27 +0530

    refactor:view

--- src/view/index.js
+++ src/view/index.js
@@ -1,10 +1,6 @@
-import { Cart } from "./Cart/CartPage";
-import { Products } from "./Products/ProductsPage";
-import { ProductDetail } from "./Products/ProductDetailPage";
-import { Wishlist } from "./WishList/WishListPage";
 import { Login } from "./Auth/Login";
 import { User } from "./User";
 import { Home } from "./Home";
 import { Signup } from "./Auth/Signup";
 
-export { Cart, Products, Wishlist, Login, ProductDetail, User, Home, Signup };
+export { Login, User, Home, Signup };
