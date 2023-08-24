d4b03fd4b1cd670880e31132ecf89c2af5ecc8ba
ra-kesh <iamrakeshkumar@pm.me>
2023-08-25 00:03:42 +0530

    refactor:hides the home route

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ function App() {
       <ErrorBoundary>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path="/products" element={<Products />} />
+            <Route path="/" element={<Products />} />
             <Route path="/products/:productId" element={<ProductDetail />} />
             <PrivateRoute path="/cart" element={<Cart />} />
             <PrivateRoute path="/wishlist" element={<Wishlist />} />
             <PrivateRoute path="/user" element={<User />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
           </Routes>
         </Suspense>
       </ErrorBoundary>
