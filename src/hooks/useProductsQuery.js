d046ffdc8d70da3e6faeaaee00ac72c4cd171fd5
ra-kesh <iamrakeshkumar@pm.me>
2023-08-25 23:26:55 +0530

    refactor:added limit and skip with changed response

--- src/hooks/useProductsQuery.js
+++ src/hooks/useProductsQuery.js
@@ -2,16 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { apiUrl } from "../constants";
 
-const useProductsQuery = () => {
+const useProductsQuery = (limit = 9, skip = 0) => {
   const fetchProducts = async () => {
-    const {
-      data: { data: products },
-    } = await axios.get(`${apiUrl}/products`);
-    return products;
+    const { data: response } = await axios.get(
+      `${apiUrl}/products?limit=${limit}&skip=${skip}`
+    );
+
+    return response;
   };
 
   return useQuery({
-    queryKey: ["products"],
+    queryKey: ["products", limit, skip],
     queryFn: () => fetchProducts(),
     retry: 3,
     cacheTime: 24 * 60 * 1000,
