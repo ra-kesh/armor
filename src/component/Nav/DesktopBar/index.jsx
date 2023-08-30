aab1b27a51374bfb8d80cb7e017d285541a5474c
ra-kesh <iamrakeshkumar@pm.me>
2023-08-30 19:47:18 +0530

    style:desktop bar css

--- src/component/Nav/DesktopBar/index.jsx
+++ src/component/Nav/DesktopBar/index.jsx
@@ -1,27 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth, useUserData } from "../../../hooks";
 import style from "./DesktopBar.module.css";
 
 const DesktopBar = () => {
   const { userInfo } = useAuth();
+  const location = useLocation();
   const { cartList, wishList } = useUserData();
 
   return (
     <ul className="block">
       <Link to="/">
-        <li className={style.desktop_bar_item}>Shop</li>
-      </Link>
-      <Link to={userInfo ? "/user" : "/login"}>
-        <li className={style.desktop_bar_item}>Account</li>
+        <li
+          className={
+            location.pathname === "/"
+              ? style.desktop_bar_item_active
+              : style.desktop_bar_item
+          }
+        >
+          Products
+        </li>
       </Link>
       <Link to={userInfo ? "/wishlist" : "/login"}>
-        <li className={style.desktop_bar_item}>
+        <li
+          className={
+            location.pathname === "/wishlist"
+              ? style.desktop_bar_item_active
+              : style.desktop_bar_item
+          }
+        >
           Wishlist ({wishList.length}){" "}
         </li>
       </Link>
       <Link to={userInfo ? "/cart" : "/login"}>
-        <li className={style.desktop_bar_item}>Cart ({cartList.length})</li>
+        <li
+          className={
+            location.pathname === "/cart"
+              ? style.desktop_bar_item_active
+              : style.desktop_bar_item
+          }
+        >
+          Cart ({cartList.length})
+        </li>
+      </Link>
+      <Link to={userInfo ? "/user" : "/login"}>
+        <li
+          className={
+            location.pathname === "/user"
+              ? style.desktop_bar_item_active
+              : style.desktop_bar_item
+          }
+        >
+          User
+        </li>
       </Link>
     </ul>
   );
