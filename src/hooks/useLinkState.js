ad806739c93c5cace503dadc5a1ea73cd132bfd3
ra-kesh <iamrakeshkumar@pm.me>
2023-09-09 23:57:00 +0530

    feat:added update query param function

--- src/hooks/useLinkState.js
+++ src/hooks/useLinkState.js
@@ -1,20 +1,40 @@
+import { useMemo } from "react";
 import { useLocation } from "react-router-dom";
 
 const useLinkState = () => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
+  const { pathname, search } = useLocation();
+  const searchParams = useMemo(() => new URLSearchParams(search), [search]);
   const category = searchParams.get("category") || "all";
   const sort = searchParams.get("sort") || "default";
-  const page = parseInt(searchParams.get("page")) || 1;
-  const perPage = parseInt(searchParams.get("per_page")) || 6;
-  const path = location.pathname + location.search;
+  const page = useMemo(
+    () => parseInt(searchParams.get("page")) || 1,
+    [searchParams]
+  );
+
+  const perPage = useMemo(
+    () => parseInt(searchParams.get("per_page")) || 6,
+    [searchParams]
+  );
+
+  const updateQueryParam = (paramName, paramValue) => {
+    if (paramName === "category") {
+      const newSearchParams = new URLSearchParams();
+      newSearchParams.set(paramName, paramValue);
+      return `${pathname}?${newSearchParams.toString()}`;
+    } else {
+      searchParams.set(paramName, paramValue);
+      return `${pathname}?${searchParams.toString()}`;
+    }
+  };
 
   return {
-    path,
+    path: `${pathname}${search}`,
     category,
     sort,
     page,
     perPage,
+    searchParams,
+    updateQueryParam,
   };
 };
 
